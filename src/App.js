98b7147558445c7d6be8f635c2cb22c708cb74f3
Gerardo Andree Pérez Cabrera <88795594+gperez2018345@users.noreply.github.com>
2022-04-05 19:20:21 -0600

    proyecto1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      contador : 0
+    }; 
+  }
+
+  agregar(){
+    this.setState({
+       contador : this.state.contador+1
+    });
+  }
+  restar(){
+    this.setState({
+       contador : this.state.contador-1
+    });
+  }
+
+  resetear=()=>{
+    this.setState({contador:0})
+  }
+  
+
+  render(){
+    return (
+      <div className="App-header">
+      <div className="cont"> 
+       <Contador valor={this.state.contador}/>        
+        <div >
+          <div>
+          <Button label="Add" onClick={()=>this.agregar()} />
+          <Button label="Rest" onClick={()=>this.restar()} />
+          <Button label="Restart" onClick={()=>this.resetear()} />
+          </div>
+        </div>
+          
+      </div>
+      </div>
+    )
+  }
+
 }
+
+
+//Functional Component
+const Contador = props => {
+
+  return(
+    <div>    
+      <h1 className={props.valor < 0 ? 'negative':'positive'}>{props.valor}</h1>
+    </div>
+    
+  );
+};
+const Button = props => {
+    return (
+      <button className="button" onClick={()=>props.onClick()}> {props.label}</button>
+    );
+};
+
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
+
 
 export default App;
